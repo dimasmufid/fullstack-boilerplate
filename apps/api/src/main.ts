2a669cdbd893e13c383735e40170c405e93f5c2d
Dimas Abdullah Mufid <dimasmoveit@gmail.com>
2026-02-14 12:15:58 +0700

    Access NestJS Swagger docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api/v1/docs', app, swaggerDocument);
+  SwaggerModule.setup('docs', app, swaggerDocument);
 
   await app.listen(process.env.PORT ?? 4000);
 }
