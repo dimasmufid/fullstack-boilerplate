b45e3fe089353548707e65016762f0a3bc48959b
Dimas Abdullah Mufid <dimasmoveit@gmail.com>
2026-02-14 12:42:10 +0700

    hide root route from swagger docs

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 import { AppService } from './app.service';
 
@@ -8,6 +9,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiExcludeEndpoint()
   getHello(): string {
     return this.appService.getHello();
   }
